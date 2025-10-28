bf90d3b7c9456ef5c678ed0298bbb8743e32f3df
Maxx Rodriguez <mrodriguez27389@columbushs.com>
2025-10-28 10:20:55 -0400

    finishing 1.2.1

--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
@@ -3,10 +3,24 @@
 */
 
 // TODO 1: Create variables age (34) and price (12.99), print both
+let age = 34;
+let price = 12.99;
+console.log (`Age: ${age}`)
+console.log ("Price: "+ price)
+
 // TODO 1b: Divide "Sandra" by 2 and print result (expect NaN)
+let whatIsThis = "Sandra" / 2;
+console.log(whatIsThis);
 
 // TODO 2: Print basic math ops: + - * /
+console.log ("2 + 2 =", 2 + 2);
+console.log ("4 - 2 =", 4 - 2);
+console.log ("3 * 2 =", 3 * 2);
+console.log ("6 / 2 =", 6 / 2);
+
 // TODO 2b: Use exponentiation (2 ** 3) and modulo (7 % 2)
+console.log (2 ** 3);
+console.log (7 % 2);
 
 // TODO 3: Assignment operators with score variable
 
